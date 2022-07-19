11ae40a160252fc86141afbaf0178323ab62041a
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2022-07-19 10:18:35 +0200

    first commit

---
+++ esbuild.js
@@ -0,0 +1,16 @@
+const [command] = process.argv.slice(2);
+
+const PROD = command === "build";
+const WATCH = command === "watch";
+
+require("esbuild").build({
+    entryPoints: ["src/ts/index.ts"],
+    outfile: "www/static/js/main.js",
+    minify: PROD,
+    bundle: true,
+    platform: "browser",
+    target: "es2016",
+    sourcemap: !PROD,
+    watch: WATCH,
+    color: true,
+});
